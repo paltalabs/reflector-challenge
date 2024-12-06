ea6cd169ae68b028a7ad400c2685f49de90ff16f
chopan123 <chopancatrileo@gmail.com>
2024-12-06 12:29:28 -0300

    utils file

--- contract/src/model.rs
+++ contract/src/model.rs
@@ -32,3 +32,67 @@ pub struct Config {
     pub oracle: Address,
     pub asset_ratios: Vec<AssetRatio>,
 }
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Instruction {
+    pub action: ActionType,
+    pub strategy: Option<Address>,
+    pub amount: Option<i128>,
+    pub swap_details_exact_in: OptionalSwapDetailsExactIn,
+    pub swap_details_exact_out: OptionalSwapDetailsExactOut,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq, Copy)]
+pub enum ActionType {
+    Withdraw = 0,
+    Invest = 1,
+    SwapExactIn = 2,
+    SwapExactOut = 3,
+    Zapper = 4,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct SwapDetailsExactIn {
+    pub token_in: Address,
+    pub token_out: Address,
+    pub amount_in: i128,
+    pub amount_out_min: i128,
+    pub distribution: Vec<DexDistribution>,
+    pub deadline: u64,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct SwapDetailsExactOut {
+    pub token_in: Address,
+    pub token_out: Address,
+    pub amount_out: i128,
+    pub amount_in_max: i128,
+    pub distribution: Vec<DexDistribution>,
+    pub deadline: u64,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct DexDistribution {
+    pub protocol_id: String,
+    pub path: Vec<Address>,
+    pub parts: u32,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum OptionalSwapDetailsExactIn {
+    Some(SwapDetailsExactIn),
+    None,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum OptionalSwapDetailsExactOut {
+    Some(SwapDetailsExactOut),
+    None,
+}
