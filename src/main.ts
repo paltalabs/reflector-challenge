4d2da4dddcd102128311381ee8078783a55a98c1
coderipper <hello@joaquinsoza.dev>
2024-12-06 11:06:04 -0300

    updated soroban-toolkit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { toolkitLoader } from "./toolkit";
+import { deployContract } from "soroban-toolkit";
 
 async function main() {
   const toolkit = toolkitLoader.getNetworkToolkit("testnet");
