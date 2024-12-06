37562865973cbbd90f844d3b71f5b36c505a373c
coderipper <hello@joaquinsoza.dev>
2024-12-06 11:25:53 -0300

    updated token contract keys

--- src/mintAndCreateLP.ts
+++ src/mintAndCreateLP.ts
@@ -1,24 +1,25 @@
 import "dotenv/config";
-import { deploySorobanToken, invokeContract, invokeCustomContract } from "soroban-toolkit";
-import { toolkitLoader,  } from "./toolkit";
-import { Address, Keypair, nativeToScVal, xdr } from '@stellar/stellar-sdk';
-import fs from 'fs';
-
-
+import {
+  deploySorobanToken,
+  invokeContract,
+  invokeCustomContract,
+} from "soroban-toolkit";
+import { toolkitLoader } from "./toolkit";
+import { Address, Keypair, nativeToScVal, xdr } from "@stellar/stellar-sdk";
+import fs from "fs";
 
 async function main() {
   const toolkit = toolkitLoader.getNetworkToolkit("testnet");
   // The contractKey for the XRP Token is "soroban_token"
   // params is params: xdr.ScVal[],
   // mintParams [from, to, amount]
- 
- 
-  // will mint 1,000 units of XRP = 1,000.0000000 = 10000000000
-  const mintTo = new Address(toolkit.admin.publicKey()).toScVal()
-  const mintAmount = nativeToScVal(10000000000, { type: 'i128' });
-  const mintParams: xdr.ScVal[] = [mintTo, mintAmount]
 
-  await invokeContract(toolkit, "soroban_token", "mint", mintParams);
+  // will mint 1,000 units of XRP = 1,000.0000000 = 10000000000
+  const mintTo = new Address(toolkit.admin.publicKey()).toScVal();
+  const mintAmount = nativeToScVal(10000000000, { type: "i128" });
+  const mintParams: xdr.ScVal[] = [mintTo, mintAmount];
+
+  await invokeContract(toolkit, "XRP", "mint", mintParams);
 
   // at the time of writing
   // 1 XRP = $2.3 ==> 1 USD = 0.43 XRP
@@ -30,9 +31,11 @@ async function main() {
 
   // Get Soroswap Router Address from public/testnet.soroswap.json using fs.readFileSync
 
-  const network = "testnet"
+  const network = "testnet";
   // should use fs.readFileSync to get the router address from thje json
-  const addressesJson = fs.readFileSync(`./public/${network}.contracts.json`).toString();
+  const addressesJson = fs
+    .readFileSync(`./public/${network}.contracts.json`)
+    .toString();
   const addresses = JSON.parse(addressesJson);
   const routerAddress = addresses.soroswap_router;
   console.log("routerAddress", routerAddress);
@@ -51,12 +54,12 @@ async function main() {
 
   // xlm address is in public
 
-  const timestamp2030= 1922796601;
+  const timestamp2030 = 1922796601;
 
   // Add liquidity
   const addLiquidityScValParams: xdr.ScVal[] = [
     new Address(addresses.xlm).toScVal(),
-    new Address(toolkit.addressBook.getContractId("soroban_token")).toScVal(),
+    new Address(toolkit.addressBook.getContractId("XRP")).toScVal(),
     nativeToScVal(2170000000, { type: "i128" }),
     nativeToScVal(430000000, { type: "i128" }),
     nativeToScVal(0, { type: "i128" }),
@@ -64,9 +67,12 @@ async function main() {
     new Address(toolkit.admin.publicKey()).toScVal(),
     nativeToScVal(timestamp2030, { type: "u64" }),
   ];
-  invokeCustomContract(toolkit, routerAddress, "add_liquidity", addLiquidityScValParams);
-
-  
+  invokeCustomContract(
+    toolkit,
+    routerAddress,
+    "add_liquidity",
+    addLiquidityScValParams
+  );
 }
 
 main();
