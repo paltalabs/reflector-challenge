f1440d360a98ec865bc850e2e01814020f322b69
coderipper <hello@joaquinsoza.dev>
2024-12-06 12:18:44 -0300

    update

--- contract/src/oracle.rs
+++ contract/src/oracle.rs
@@ -1,8 +1,21 @@
-use crate::model::AssetRatio;
-use soroban_sdk::{contractimport, Address, Env, Symbol};
+use soroban_sdk::{Env, Symbol};
+
+use crate::{model::AssetRatio, storage::get_config};
+
+soroban_sdk::contractimport!(
+    file = "src/reflector.wasm"
+);
+
+pub type OracleClient<'a> = Client<'a>;
 
 pub fn get_price(e: &Env, asset: &AssetRatio) -> i128 {
+    let config = get_config(e);
+    let oracle = config.oracle;
+
+    let oracleClient = OracleClient::new(&e, &oracle);
+    
     if asset.symbol == Symbol::new(e, "XRP") {
+
         i128::from(2_2_698_160) // Price for XRP
     } else if asset.symbol == Symbol::new(e, "XLM") {
         i128::from(4_578_725) // Price for XLM
