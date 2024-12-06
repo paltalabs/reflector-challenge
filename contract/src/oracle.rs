e2f5eae30c3f9fe81ee052f50ae82b6d95ec6775
chopan123 <chopancatrileo@gmail.com>
2024-12-06 11:42:37 -0300

    more harcoded price

--- contract/src/oracle.rs
+++ contract/src/oracle.rs
@@ -1,5 +1,12 @@
+use crate::model::AssetRatio;
 use soroban_sdk::{contractimport, Address, Env};
 
-pub fn get_price(e: &Env, asset: &Address) -> i128 {
-    i128::from(2_2_698_160)
+pub fn get_price(e: &Env, asset: &AssetRatio) -> i128 {
+    if asset.symbol == Symbol::new(e, "XRP") {
+        i128::from(2_2_698_160) // Price for XRP
+    } else if asset.symbol == Symbol::new(e, "XLM") {
+        i128::from(4_578_725) // Price for XLM
+    } else {
+        panic!("Unsupported asset symbol")
+    }
 }
