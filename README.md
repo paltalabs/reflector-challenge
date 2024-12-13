e1e9fcccd6d152f898ce996aa9d9b4a0d66ef871
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-12-13 20:13:44 -0300

    ✨Add deploy script for testnet, main script and readme.md

--- README.md
+++ README.md
@@ -1,29 +1,59 @@
-Set up the environment variables
+# Trustless rebalancer:
 
-```
-cp .env.example .env
-```
+## Setting up your local instance:
+If you want to try trustless rebalancer for yourself you need to:
 
-Fill the .env file with the admin private key and the aggregator address.
+### Clone the repo:
+In a new terminal run: `git clone https://github.com/paltalabs/reflector-challenge`
+and navigate to the new folder. 
 
-Run in this order
+    cd reflector-challenge
+### Run the container:
+(If it´s your first time using a soroban docker image this step will take a while.)
 
-```
-docker compose up -d
-bash run.sh
-yarn
+    docker compose up -d && bash run.sh
+
+### Set up the env:
+Copy the .env.example and create a new .env file in the root folder of the project:
+
+    cp .env.example .env
+
+And fill the admin secret key using a soroban secret key format string, and the aggregator addresses using a soroban public address fromat string.
+
+### Install dependencies:
+Install the project dependencies using yarn:
+
+    yarn install
+
+### Build and deploy the contracts:
+In order to deploy your smart contracts you should navigate to the contracts folder and build the files running:
+
+     cd contracts && make build
+
+Once this process has finished you can deploy each contract running in this exact order: 
+
+ ``` bash
 yarn deployXRPToken # Create XRP Soroban Token and save address in .soroban folder
 yarn mintAndCreateLP # Mint XRP Tokens and create LP with native Testnet XLM in Soroswap
 yarn deployHodl # Deploy Hodl strategies for XRP & XLM contracts and save address in .soroban folder
 yarn deployFactory # Deploy the defindex factory
 yarn deployVault # Deploys a vault with 2 assets and one strategy each
+ ```
+
+Or deploy them all at once by running:
+
+    yarn deployTestnet
+
+## Testing the trustless rebalancer:
+Once you have deployed all the contracts you can run
+``` bash
+    #usage: yarn start <network>
+    yarn start testnet
 ```
-
-Now you can Swap Tokens on Soroswap in
-https://app.soroswap.finance/swap/CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC/{YOUR DEPLOYED XRP ADDRESS}
-
-Make sure select "Testnet" on your Freighter Wallet
-
-In contracts folder we have:
-- reflector.wasm, compiled from https://github.com/reflector-network/reflector-contract
-
+This script will:
+	- Mint XLM and XRP to a test account.
+	- Deposit XLM and XRP into a defindex vault.
+	- Invest XLM and XRP in their own HODL strategies.
+	- Set the Trustless Manager contract as the vault manager.
+	- Call the rebalance method in the vault.
+	- Display a table with the results.
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,128 @@
+import { Address, Asset, Keypair, nativeToScVal, scValToNative, xdr } from "@stellar/stellar-sdk";
 import { toolkitLoader } from "./toolkit";
-import { deployContract } from "soroban-toolkit";
+import { invokeContract, airdropAccount } from "soroban-toolkit";
+
+
+const args = process.argv.slice(2);
+if(args.length !== 1){
+  console.error("Usage: yarn start <network>");
+  process.exit(1);
+}
+
+
+const network = args[0];
+const toolkit = toolkitLoader.getNetworkToolkit(network);
+const xrp_token = new Address(toolkit.addressBook.getContractId("XRP"));
 
 async function main() {
-  const toolkit = toolkitLoader.getNetworkToolkit("testnet");
+  const vaultAddress = toolkit.addressBook.getContractId("vault");
+  console.log("-------------------------------------------------------");
+  console.log("Vault address:", vaultAddress);
+  console.log("-------------------------------------------------------");
+  
+  //Mintear XRP y XLM para el usuario 100_0_000_000
+  const newUser = Keypair.random()
+  const mintAmount = nativeToScVal(100_0_000_000, { type: "i128" });
+  const mintParams: xdr.ScVal[] = [new Address(newUser.publicKey()).toScVal(), mintAmount];
 
-  console.log(toolkit);
+  await invokeContract(toolkit, "XRP", "mint", mintParams);
+  console.log("Minted XRP to user:", newUser.publicKey().toString());
+  await airdropAccount(toolkit, newUser);
+
+
+  //Deposit XRP y XLM al vault
+
+  const depositAmount = nativeToScVal(100_000_000, { type: "i128" });
+  let amountsDesired = [depositAmount, depositAmount];
+  let amountsMin = [depositAmount, depositAmount];
+  const depositParams: xdr.ScVal[] = [
+    xdr.ScVal.scvVec(amountsDesired.map((amount) => nativeToScVal(amount, { type: "i128" }))),
+    xdr.ScVal.scvVec(amountsMin.map((min) => nativeToScVal(min, { type: "i128" }))),
+    new Address(newUser.publicKey()).toScVal(),
+    xdr.ScVal.scvBool(false)
+  ];
+
+  await invokeContract(toolkit, "vault", "deposit", depositParams, false, newUser);
+  
+  const investmentArgs: any[] = [
+    {
+      asset: xrp_token,
+      strategy_investments: [
+        {
+          strategy: new Address(toolkit.addressBook.getContractId("hodl_xrp")),
+          amount: 100_000_000,
+        },
+      ],
+    },
+    {
+      asset: new Address(Asset.native().contractId(toolkit.passphrase)),
+      strategy_investments: [
+        {
+          strategy: new Address(toolkit.addressBook.getContractId("hodl_xlm")),
+          amount: 100_000_000,
+        },
+      ],
+    },
+  ];
+
+  const mappedParam = xdr.ScVal.scvVec(
+    investmentArgs.map((entry) =>
+        xdr.ScVal.scvMap([
+            new xdr.ScMapEntry({
+                key: xdr.ScVal.scvSymbol("asset"),
+                val: entry.asset.toScVal(), // Convert asset address to ScVal
+            }),
+            new xdr.ScMapEntry({
+                key: xdr.ScVal.scvSymbol("strategy_allocations"),
+                val: xdr.ScVal.scvVec(
+                    entry.strategy_investments.map((investment: any) =>
+                        xdr.ScVal.scvMap([
+                            new xdr.ScMapEntry({
+                                key: xdr.ScVal.scvSymbol("amount"),
+                                val: nativeToScVal(BigInt(investment.amount), { type: "i128" }), // Ensure i128 conversion
+                            }),
+                            new xdr.ScMapEntry({
+                                key: xdr.ScVal.scvSymbol("strategy_address"),
+                                val: investment.strategy.toScVal(), // Convert strategy address
+                            }),
+                        ])
+                    )
+                ),
+            }),
+        ])
+    )
+  );
+
+  await invokeContract(toolkit, "vault", "invest", [mappedParam], false, toolkit.admin);
+
+  const invested_funds_after_investment = await invokeContract(toolkit, "vault", "fetch_current_invested_funds", [], true);
+  const parsed_invested_funds_after_investment = scValToNative(invested_funds_after_investment.result.retval);
+  console.log(parsed_invested_funds_after_investment)
+
+  //Set tmanager as vault manager
+  const tmanagerAddress = toolkit.addressBook.getContractId("tmanager");
+  const setAdminParams: xdr.ScVal[] = [new Address(tmanagerAddress).toScVal()];
+  const set_manager = await invokeContract(toolkit, "vault", "set_manager", setAdminParams);
+  console.log("Set tmanager as admin of vault", set_manager);
+
+  //Rebalance
+  await invokeContract(toolkit, "tmanager", "rebalance", [], false, newUser);
+
+  const invested_funds_after_rebalance = await invokeContract(toolkit, "vault", "fetch_current_invested_funds", [], true);
+  const parsed_invested_funds_after_rebalance = scValToNative(invested_funds_after_rebalance.result.retval);
+
+  console.log(parsed_invested_funds_after_investment.find((entry: any) => entry[0] === xrp_token)[1])
+  console.table({
+    XRP:{
+      "Before rebalance": parsed_invested_funds_after_investment,
+      "After rebalance": parsed_invested_funds_after_rebalance[0]
+    },
+    XLM:{
+      "Before rebalance": parsed_invested_funds_after_investment[1],
+      "After rebalance": parsed_invested_funds_after_investment[1]
+    }
+
+  })
 }
 
 main();
