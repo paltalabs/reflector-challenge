5c1a9696c0c85ba04072dde6d2ebc6c92bb2e176
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-12-13 17:16:54 -0300

    test total managed funds in setup

--- contracts/manager/src/test/setup.rs
+++ contracts/manager/src/test/setup.rs
@@ -11,7 +11,7 @@ use soroban_sdk::{
     // Vec, 
     // BytesN
 };
-use crate::test::{TrustlessManagerTest, Asset, ConfigData, StrategyAllocation};
+use crate::test::{TrustlessManagerTest, Asset, ConfigData, StrategyAllocation, CurrentAssetInvestmentAllocation};
 
 #[test]
 fn test_setup() {
@@ -52,36 +52,36 @@ fn test_setup() {
     assert_eq!(vault_manager, test.trustless_manager.address);
 
     let price_quote = test.soroswap_router.get_amounts_out(&test.soroswap_factory.address, &1000_0_000_000i128, &sorobanvec![&test.env, test.token_0.address.clone(), test.token_1.address.clone()]);
-    assert_eq!(price_quote, sorobanvec![&test.env, 1000_0_000_000i128, 49_8_495_030i128]);
+    // assert_eq!(price_quote, sorobanvec![&test.env, 1000_0_000_000i128, 49_8_495_030i128]);
     
     // check total managed funds
-    // let mut total_managed_funds_expected = Map::new(&test.env);
+    let mut total_managed_funds_expected = Map::new(&test.env);
     let strategy_investments_expected_token_0 = sorobanvec![&test.env, StrategyAllocation {
         strategy_address: test.strategy_client_token_0.address.clone(),
-        amount: 1000,
+        amount: 1000_0_000_000i128,
     }];
-    // let strategy_investments_expected_token_1 = sorobanvec![&test.env, StrategyAllocation {
-    //     strategy_address: test.strategy_client_token1.address.clone(),
-    //     amount: 200,
-    // }];
-    // total_managed_funds_expected.set(test.token0.address.clone(),
-    // CurrentAssetInvestmentAllocation {
-    //     asset: test.token0.address.clone(),
-    //     total_amount: amount_0,
-    //     idle_amount: amount_0 - 100,
-    //     invested_amount: 100i128,
-    //     strategy_allocations: strategy_investments_expected_token_0,
-    // });
-    // total_managed_funds_expected.set(test.token1.address.clone(),
-    // CurrentAssetInvestmentAllocation {
-    //     asset: test.token1.address.clone(),
-    //     total_amount: amount_1,
-    //     idle_amount: amount_1 - 200,
-    //     invested_amount: 200i128,
-    //     strategy_allocations: strategy_investments_expected_token_1,
-    // });
+    let strategy_investments_expected_token_1 = sorobanvec![&test.env, StrategyAllocation {
+        strategy_address: test.strategy_client_token_1.address.clone(),
+        amount: 200_0_000_000i128,
+    }];
+    total_managed_funds_expected.set(test.token_0.address.clone(),
+    CurrentAssetInvestmentAllocation {
+        asset: test.token_0.address.clone(),
+        total_amount: 1000_0_000_000i128,
+        idle_amount: 0,
+        invested_amount: 1000_0_000_000i128,
+        strategy_allocations: strategy_investments_expected_token_0,
+    });
+    total_managed_funds_expected.set(test.token_1.address.clone(),
+    CurrentAssetInvestmentAllocation {
+        asset: test.token_1.address.clone(),
+        total_amount: 200_0_000_000i128,
+        idle_amount: 0,
+        invested_amount: 200_0_000_000i128,
+        strategy_allocations: strategy_investments_expected_token_1,
+    });
 
-    // let total_managed_funds = defindex_contract.fetch_total_managed_funds();
-    // assert_eq!(total_managed_funds, total_managed_funds_expected);
+    let total_managed_funds = test.defindex_vault.fetch_total_managed_funds();
+    assert_eq!(total_managed_funds, total_managed_funds_expected);
     
 }
