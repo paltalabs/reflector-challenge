cf0e245c9698f82aa5d1d55c64db3ffe3591ccdd
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-12-13 12:24:00 -0300

    factory is tested

--- contracts/manager/src/test.rs
+++ contracts/manager/src/test.rs
@@ -3,7 +3,15 @@ extern crate std;
 use soroban_sdk::token::{
     StellarAssetClient as SorobanTokenAdminClient, TokenClient as SorobanTokenClient,
 };
-use soroban_sdk::{testutils::Address as _, vec as sorobanvec, Address, Env, String, Val, Vec};
+use soroban_sdk::{
+    testutils::Address as _, 
+    vec as sorobanvec, 
+    Address, 
+    Env, 
+    String, 
+    Val, 
+    Vec, 
+    BytesN};
 use std::vec;
 
 use crate::{TrustlessManager, TrustlessManagerClient};
@@ -33,13 +41,30 @@ use hodl_strategy::HodlStrategyClient;
 use reflector::ReflectorClient;
 
 // Deploy Contracts
-fn create_defindex_vault<'a>(e: &Env) -> DeFindexVaultClient<'a> {
-    // let address = &e.register_contract_wasm(None, defindex_vault::WASM);
-    let address = &e.register(defindex_vault::WASM, ());
-    DeFindexVaultClient::new(e, address)
-}
-fn create_defindex_factory<'a>(e: &Env) -> DeFindexFactoryClient<'a> {
-    // let address = &e.register_contract_wasm(None, defindex_factory::WASM);
+// fn create_defindex_vault<'a>(e: &Env) -> DeFindexVaultClient<'a> {
+//     // let address = &e.register_contract_wasm(None, defindex_vault::WASM);
+//     let args = (asset.clone(), init_args.clone());
+
+//     let address = &e.register(hodl_strategy::WASM, args);
+//     let address = &e.register(defindex_vault::WASM, ());
+//     DeFindexVaultClient::new(e, address)
+// }
+// fn create_defindex_factory<'a>(e: &Env, admin: &Address, defindex_receiver: &Address, defindex_fee: u32, defindex_wasm_hash: &BytesN<32>) -> DeFindexFactoryClient<'a> {
+//     let args = (admin, defindex_receiver, defindex_fee, defindex_wasm_hash);
+//     DeFindexFactoryClient::new(e, &e.register(DeFindexFactory, args))
+// }
+fn create_defindex_factory<'a>(
+    e: &Env, 
+    admin: &Address, 
+    defindex_receiver: &Address, 
+    defindex_fee: u32,
+    defindex_wasm_hash: &BytesN<32>) -> DeFindexFactoryClient<'a> {
+    
+    let args = (
+        admin, 
+        defindex_receiver, 
+        defindex_fee, 
+        defindex_wasm_hash);
     let address = &e.register(defindex_factory::WASM, ());
     DeFindexFactoryClient::new(e, address)
 }
@@ -51,6 +76,10 @@ fn create_hodl_strategy<'a>(e: &Env, asset: &Address) -> HodlStrategyClient<'a> 
     let hodl_strategy = HodlStrategyClient::new(e, address);
     hodl_strategy
 }
+fn create_reflector<'a>(e: &Env) -> ReflectorClient<'a> {
+    let address = &e.register(reflector::WASM, ());
+    ReflectorClient::new(e, address)
+}
 
 // THE CONTRACT TO BE TESTED
 fn create_trustless_manager<'a>(e: &Env) -> TrustlessManagerClient<'a> {
@@ -58,7 +87,6 @@ fn create_trustless_manager<'a>(e: &Env) -> TrustlessManagerClient<'a> {
     TrustlessManagerClient::new(e, address)
     
 }
-
 
 // TOKEN RELATED FUNCTIONS
 pub(crate) fn create_token_contract<'a>(e: &Env, admin: &Address) -> SorobanTokenClient<'a> {
@@ -97,10 +125,20 @@ pub(crate) fn get_token_admin_client<'a>(
 //     ]
 // }
 
+pub(crate) fn generate_random_users(e: &Env, users_count: u32) -> vec::Vec<Address> {
+        let mut users = vec![];
+        for _c in 0..users_count {
+            users.push(Address::generate(e));
+        }
+        users
+}
+
 pub struct TrustlessManagerTest<'a> {
     env: Env,
-    defindex_factory: Address,
-    defindex_vault: DeFindexVaultClient<'a>,
+    defindex_factory: DeFindexFactoryClient<'a>,
+    // defindex_vault: DeFindexVaultClient<'a>,
+    admin: Address,
+    defindex_receiver: Address,
     token_0_admin_client: SorobanTokenAdminClient<'a>,
     token_0: SorobanTokenClient<'a>,
     token_0_admin: Address,
@@ -110,9 +148,10 @@ pub struct TrustlessManagerTest<'a> {
     emergency_manager: Address,
     vault_fee_receiver: Address,
     defindex_protocol_receiver: Address,
-    // manager: Address,
     strategy_client_token_0: HodlStrategyClient<'a>,
     strategy_client_token_1: HodlStrategyClient<'a>,
+    reflector: ReflectorClient<'a>,
+    trustless_manager: TrustlessManagerClient<'a>,
 }
 
 impl<'a> TrustlessManagerTest<'a> {
@@ -120,33 +159,46 @@ impl<'a> TrustlessManagerTest<'a> {
         let env = Env::default();
         env.mock_all_auths();
 
-        let defindex_factory = Address::generate(&env);
-        let defindex_vault = create_defindex_vault(&env);
-
-        let emergency_manager = Address::generate(&env);
-        let vault_fee_receiver = Address::generate(&env);
-        let defindex_protocol_receiver = Address::generate(&env);
-        let manager = Address::generate(&env);
-
+        // DEFINDEX FACTORY
+        let admin = Address::generate(&env);
+        let defindex_receiver = Address::generate(&env);
+        let defindex_vault_wasm_hash = env.deployer().upload_contract_wasm(defindex_vault::WASM);
+        let factory_contract = create_defindex_factory(
+            &env, 
+            &admin, 
+            &defindex_receiver, 
+            100u32, 
+            &defindex_vault_wasm_hash);
+            
+        // TEST TOKENS
         let token_0_admin = Address::generate(&env);
         let token_1_admin = Address::generate(&env);
         
         let token_0 = create_token_contract(&env, &token_0_admin);
         let token_1 = create_token_contract(&env, &token_1_admin);
-
+        
         let token_0_admin_client = get_token_admin_client(&env, &token_0.address.clone());
         let token_1_admin_client = get_token_admin_client(&env, &token_1.address.clone());
-
-
-        let strategy_client_token_0 = create_hodl_strategy(&env, &token_0.address);
-        let strategy_client_token_1 = create_hodl_strategy(&env, &token_1.address);
-
+            
+            let strategy_client_token_0 = create_hodl_strategy(&env, &token_0.address);
+            let strategy_client_token_1 = create_hodl_strategy(&env, &token_1.address);
+            
+            let reflector = create_reflector(&env);
+            let trustless_manager = create_trustless_manager(&env);
+            
+            // DEFINDEX PROTOCOL
+            
+            let emergency_manager = Address::generate(&env);
+            let vault_fee_receiver = Address::generate(&env);
+            let defindex_protocol_receiver = Address::generate(&env);
+            let manager = Address::generate(&env);
         env.budget().reset_unlimited();
-
         TrustlessManagerTest {
             env,
             defindex_factory,
-            defindex_vault,
+            admin,
+            defindex_receiver,
+            // defindex_vault,
             token_0_admin_client,
             token_0,
             token_0_admin,
@@ -159,17 +211,12 @@ impl<'a> TrustlessManagerTest<'a> {
 //             manager,
             strategy_client_token_0,
             strategy_client_token_1,
+            reflector,
+            trustless_manager,
         }
     }
-
-    //     pub(crate) fn generate_random_users(e: &Env, users_count: u32) -> vec::Vec<Address> {
-    //         let mut users = vec![];
-    //         for _c in 0..users_count {
-    //             users.push(Address::generate(e));
-    //         }
-    //         users
-    //     }
 }
 
 // mod vault;
 mod utils;
+mod setup;
