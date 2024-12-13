bd7e42559629bb84aec37be6d5fd50828ec46b6c
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-12-13 09:06:38 -0300

    ♻️Refactor deploy vaults

--- src/deploy_vaults.ts
+++ src/deploy_vaults.ts
@@ -17,40 +17,36 @@ import { randomBytes } from "crypto";
 const network = process.argv[2];
 
 const toolkit = toolkitLoader.getNetworkToolkit("testnet");
-const soroban_token = new Address(toolkit.addressBook.getContractId("XRP"));
-
-let xlmContractId: string = Asset.native().contractId(toolkit.passphrase);
 
 export async function deployVaults() {
   if (network != "mainnet") await airdropAccount(toolkit, toolkit.admin);
   let account = await toolkit.horizonRpc.loadAccount(toolkit.admin.publicKey());
-  console.log("publicKey", toolkit.admin.publicKey());
   let balance = account.balances.filter((item) => item.asset_type == "native");
   console.log("Current Admin account balance:", balance[0].balance);
 
   console.log("-------------------------------------------------------");
   console.log("Deploying vault");
   console.log("-------------------------------------------------------");
-  await installContract(toolkit, "vault", undefined, toolkit.admin);
+
   const assets = [
-    {
-      address: toolkit.addressBook.getContractId("XRP"),
-      strategies: [
-        {
-          name: "Fixed Strategy",
-          address: toolkit.addressBook.getContractId("hodl_xrp"),
-          paused: false
-        }
-      ]
-    },
     {
       address: Asset.native().contractId(toolkit.passphrase),
       strategies: [
         {
-          name: "Hodl Strategy",
+          name: "Hodl XLM Strategy",
           address: toolkit.addressBook.getContractId("hodl_xlm"),
           paused: false
         },
+      ]
+    },
+    {
+      address: toolkit.addressBook.getContractId("XRP"),
+      strategies: [
+        {
+          name: "Hodl XRP Strategy",
+          address: toolkit.addressBook.getContractId("hodl_xrp"),
+          paused: false
+        }
       ]
     },
   ];
@@ -85,35 +81,24 @@ export async function deployVaults() {
     ]);
   });
 
-  /* 
-  fn create_defindex_vault(
-        emergency_manager: Address, 
-        fee_receiver: Address, 
-        vault_fee: u32,
-        vault_name: String,
-        vault_symbol: String,
-        manager: Address,
-        assets: Vec<AssetStrategySet>,
-        salt: BytesN<32>
-    ) 
-  */
-
   const createDeFindexParams: xdr.ScVal[] = [
     new Address(toolkit.admin.publicKey()).toScVal(),   // emergency_manager
     new Address(toolkit.admin.publicKey()).toScVal(),   // fee_receiver
     nativeToScVal(100, { type: "u32" }),                // vault_fee
-    nativeToScVal("AAA/XRP", { type: "string" }),       // vault_name
+    nativeToScVal("SampleVault", { type: "string" }),   // vault_name
     nativeToScVal("HXRM", { type: "string" }),          // vault_symbol
     new Address(toolkit.admin.publicKey()).toScVal(),   // manager
     xdr.ScVal.scvVec(assetAllocations),                 // assets
     nativeToScVal(randomBytes(32)),                     // salt
   ];
 
+  const createDefindexParamsScvec = xdr.ScVal.scvVec(createDeFindexParams);
+
   const result = await invokeContract(
     toolkit,
     "defindex_factory",
     "create_defindex_vault",
-    createDeFindexParams,
+    [createDefindexParamsScvec],
     false,
     toolkit.admin
   );
@@ -135,3 +120,31 @@ async function main() {
 }
 
 main();
+
+
+/*
+
+    '   1: [Diagnostic Event] topics:[fn_call, CCI7MPZKUBRSJ5RD7WS77BM76R34Y2BADA6PW3GVEREPXMCXEGZ65UV2, create_defindex_vault], 
+    data:[
+      [{
+        address: "CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC", 
+        strategies: [{ 
+            address: CCTDAEPPD34ZEFVFCJ5SXWL3Q2JFWTMY53SEUN5HNAE56JEHESV2CF7C, 
+            name: "Hodl XLM Strategy", 
+            paused: false
+          }]
+        }, {
+            address: "CCFDTMC25A4VGDZVJC5T4XLUTYICDBVKUVRMZJVPBZMKNTV2IQHHMVQO", 
+            strategies: [{address: CDZIA5B7YI2RHHILDT3VSWEJBOND655MXNWNB4YBXCWOHRCLSZV3FUAM, 
+            name: "Hodl XRP Strategy", 
+            paused: false
+          }]
+        }], 
+        GAGZPYKRLBXHN3O2NAWAVOP6MWIPJSQAQO3NQ42H54ZBMNF73E4YOH2E, 
+        GAGZPYKRLBXHN3O2NAWAVOP6MWIPJSQAQO3NQ42H54ZBMNF73E4YOH2E, 
+        100, 
+        "SampleVault", 
+        "HXRM", 
+        GAGZPYKRLBXHN3O2NAWAVOP6MWIPJSQAQO3NQ42H54ZBMNF73E4YOH2E, 
+        Bytes(b0d3a107e5d3407bea50c32573b1fefa02194cf703a726573df07144ee137683)]
+  */
